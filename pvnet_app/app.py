a3d4efb3794a01f218be4172084c94de90a9c95c
James Fulton <djamesfulton@yahoo.co.uk>
2023-10-25 16:25:15 +0000

    fix

--- pvnet_app/app.py
+++ pvnet_app/app.py
@@ -286,13 +286,20 @@ def app(
     # Download nwp data
     logger.info("Downloading nwp data")
     fs = fsspec.open(os.environ["NWP_ZARR_PATH"]).fs
+    fs.get(os.environ["NWP_ZARR_PATH"], "nwp.zarr", recursive=True)
     
+    ################
     # Hot fix as datapipes can't handle both lat-lon and osgb on same coord
-    fs.get(os.environ["NWP_ZARR_PATH"], "raw_nwp.zarr", recursive=True)
-    ds_nwp = xr.open_zarr("raw_nwp.zarr")
-    ds_nwp.drop_vars(("latitude", "longitude")).to_zarr("nwp.zarr")
+    ds_nwp = xr.open_zarr("nwp.zarr").compute()
+    if "latitude" in ds_nwp:
+        ds_nwp = ds_nwp.drop_vars(("latitude", "longitude"))
+    ds_nwp["variable"] = ds_nwp.variable.astype(str)
+    os.system("rm -r nwp.zarr")
+    ds_nwp.to_zarr("nwp.zarr")
+    
     del ds_nwp
-
+    ################
+    
     # ---------------------------------------------------------------------------
     # 2. Set up data loader
     logger.info("Creating DataLoader")
