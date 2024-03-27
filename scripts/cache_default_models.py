db98147888f6103281a7bbe758240ad45c89c71a
James Fulton <41546094+dfulu@users.noreply.github.com>
2024-03-27 13:20:43 +0000

    Update cache_default_models.py

--- scripts/cache_default_models.py
+++ scripts/cache_default_models.py
@@ -15,7 +15,6 @@ from pvnet_app.app import (
     default_summation_model_name,
     default_summation_model_version,
 )
-import pandas
 
 def main():
     """Download model from Huggingface and save it to cache."""
