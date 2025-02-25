92bda4140acba96b95cc35b40b84c85d338ae87b
James Fulton <djamesfulton@yahoo.co.uk>
2025-02-25 17:48:55 +0000

    clean up

--- src/pvnet_app/model_configs/pydantic_models.py
+++ src/pvnet_app/model_configs/pydantic_models.py
@@ -22,35 +22,19 @@ class Model(BaseModel):
     pvnet: ModelHF = Field(..., title="PVNet", description="The PVNet model")
     summation: ModelHF = Field(..., title="Summation", description="The Summation model")
 
-    use_adjuster: bool | None = Field(
-        False, title="Use Adjuster", description="Whether to use the adjuster model",
-    )
-    save_gsp_sum: bool | None = Field(
-        False, title="Save GSP Sum", description="Whether to save the GSP sum",
-    )
-    verbose: bool | None = Field(
-        False, title="Verbose", description="Whether to print verbose output",
-    )
-    save_gsp_to_recent: bool | None = Field(
-        False,
-        title="Save GSP to Forecast Value Last Seven Days",
+    use_adjuster: bool = Field(False, description="Whether to use the adjuster model")
+    save_gsp_sum: bool = Field(False, description="Whether to save the GSP sum")
+    verbose: bool = Field(False, description="Whether to print verbose output")
+    save_gsp_to_recent: bool = Field(
+        False, 
         description="Whether to save the GSP to Forecast Value Last Seven Days",
     )
-    day_ahead: bool | None = Field(
-        False, title="Day Ahead", description="If this model is day ahead or not",
-    )
+    day_ahead: bool = Field(False, description="If this model is day ahead or not")
+    ecmwf_only: bool = Field(False, description="If this model is only using ecmwf data")
+    uses_satellite_data: bool = Field(True, description="If this model uses satellite data")
 
-    ecmwf_only: bool | None = Field(
-        False, title="ECMWF ONly", description="If this model is only using ecmwf data",
-    )
-
-    uses_satellite_data: bool | None = Field(
-        True, title="Uses Satellite Data", description="If this model uses satellite data",
-    )
-
-    uses_ocf_data_sampler: bool | None = Field(
+    uses_ocf_data_sampler: bool = Field(
         True,
-        title="Uses OCF Data Sampler",
         description="If this model uses data sampler, old one uses ocf_datapipes",
     )
 
@@ -58,96 +42,85 @@ class Model(BaseModel):
 class Models(BaseModel):
     """A group of ml models"""
 
-    models: list[Model] = Field(
-        ..., title="Models", description="A list of models to use for the forecast",
-    )
+    models: list[Model] = Field(..., description="A list of models to use for the forecast")
 
     @field_validator("models")
     @classmethod
     def name_must_be_unique(cls, v: list[Model]) -> list[Model]:
         """Ensure that all model names are unique, respect to using ocf_data_sampler or not"""
         names = [(model.name, model.uses_ocf_data_sampler) for model in v]
-        unique_names = set(names)
 
-        if len(names) != len(unique_names):
+        if len(names) != len(set(names)):
             raise Exception(f"Model names must be unique, names are {names}")
         return v
 
 
 def get_all_models(
-    get_ecmwf_only: bool | None = False,
-    get_day_ahead_only: bool | None = False,
-    run_extra_models: bool | None = False,
-    use_ocf_data_sampler: bool | None = True,
+    allow_use_adjuster: bool = True,
+    allow_save_gsp_sum: bool = True,
+    get_ecmwf_only: bool = False,
+    get_day_ahead_only: bool = False,
+    run_extra_models: bool = False,
+    use_ocf_data_sampler: bool = True,
 ) -> list[Model]:
     """Returns all the models for a given client
 
     Args:
+        allow_use_adjuster: If set to false all models will have use_adjuster set to false
+        allow_save_gsp_sum: If set to false all models will have save_gsp_sum set to false
         get_ecmwf_only: If only the ECMWF model should be returned
-        get_day_ahead_only: If only the day ahead model should be returned
-        run_extra_models: If extra models should be run
-        use_ocf_data_sampler: If the OCF Data Sampler should be used
+        get_day_ahead_only: If only the day-ahead model should be returned
+        run_extra_models: If all extra models should be returned
+        use_ocf_data_sampler: If the ocf-data-sampler models should be returned
     """
-    try:
-        # load models from yaml file
-        filename = files("pvnet_app.model_configs").joinpath("all_models.yaml")
+    
+    filename = files("pvnet_app.model_configs").joinpath("all_models.yaml")
 
-        with fsspec.open(filename, mode="r") as stream:
-            try:
-                models_dict = parse_config(data=stream)
-                models = Models(**models_dict)
-            except Exception as config_error:
-                log.error(f"Error parsing model configuration: {config_error}")
-                raise
+    with fsspec.open(filename, mode="r") as stream:
+        try:
+            models_dict = parse_config(data=stream)
+            models = Models(**models_dict)
+        except Exception as config_error:
+            log.error(f"Error parsing model configuration: {config_error}")
+            raise config_error
 
-        models = config_pvnet_v2_model(models)
+    # Override the use_adjuster and save_gsp_sum properties
+    if not allow_use_adjuster:
+        for model in models:
+            model.use_adjuster = False
+    if not allow_save_gsp_sum:
+        for model in models:
+            model.save_gsp_sum = False
 
-        # Apply filters based on input parameters
-        filtered_models = models.models.copy()
+    # Filter models
+    filtered_models = models.models.copy()
 
-        if get_ecmwf_only:
-            log.info("Filtering for ECMWF model only")
-            filtered_models = [model for model in filtered_models if model.ecmwf_only]
+    if get_ecmwf_only:
+        log.info("Filtering for ECMWF model only")
+        filtered_models = [model for model in filtered_models if model.ecmwf_only]
 
-        if get_day_ahead_only:
-            log.info("Filtering for Day Ahead model")
-            filtered_models = [model for model in filtered_models if model.day_ahead]
-        else:
-            log.info("Excluding Day Ahead model")
-            filtered_models = [model for model in filtered_models if not model.day_ahead]
+    if get_day_ahead_only:
+        log.info("Filtering for Day Ahead model")
+        filtered_models = [model for model in filtered_models if model.day_ahead]
+    else:
+        log.info("Excluding Day Ahead model")
+        filtered_models = [model for model in filtered_models if not model.day_ahead]
 
-        if not run_extra_models and not get_day_ahead_only and not get_ecmwf_only:
-            log.info("Limiting to default pvnet_v2 model")
-            filtered_models = [model for model in filtered_models if model.name == "pvnet_v2"]
+    if not run_extra_models and not get_day_ahead_only and not get_ecmwf_only:
+        log.info("Limiting to default pvnet_v2 model")
+        filtered_models = [model for model in filtered_models if model.name == "pvnet_v2"]
 
-        if use_ocf_data_sampler:
-            log.info("Filtering for models using OCF Data Sampler")
-            filtered_models = [model for model in filtered_models if model.uses_ocf_data_sampler]
-        else:
-            log.info("Filtering for models not using OCF Data Sampler")
-            filtered_models = [model for model in filtered_models if not model.uses_ocf_data_sampler]
+    if use_ocf_data_sampler:
+        log.info("Filtering for models using OCF Data Sampler")
+        filtered_models = [model for model in filtered_models if model.uses_ocf_data_sampler]
+    else:
+        log.info("Filtering for models not using OCF Data Sampler")
+        filtered_models = [model for model in filtered_models if not model.uses_ocf_data_sampler]
 
-        selected_model_info = [
-            (model.name, f"uses_ocf_data_sampler={model.uses_ocf_data_sampler}")
-            for model in filtered_models
-        ]
-        log.info(f"Selected models: {selected_model_info}")
+    selected_model_info = [
+        (model.name, f"uses_ocf_data_sampler={model.uses_ocf_data_sampler}")
+        for model in filtered_models
+    ]
+    log.info(f"Selected models: {selected_model_info}")
 
-        return filtered_models
-
-    except Exception as e:
-        log.error(f"Critical error in get_all_models: {e}")
-        raise
-
-
-def config_pvnet_v2_model(models):
-    """Function to adjust pvnet model"""
-    # special case for environment variables
-    use_adjuster = os.getenv("USE_ADJUSTER", "true").lower() == "true"
-    save_gsp_sum = os.getenv("SAVE_GSP_SUM", "false").lower() == "true"
-    # find index where name=pvnet_v2
-    pvnet_v2_index = 0
-    models.models[pvnet_v2_index].use_adjuster = use_adjuster
-    models.models[pvnet_v2_index].save_gsp_sum = save_gsp_sum
-
-    return models
+    return filtered_models
