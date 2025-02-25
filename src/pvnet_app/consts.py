aee91dc9e5e03fd2f3134fd018d59724eed89e2f
James Fulton <djamesfulton@yahoo.co.uk>
2025-02-25 16:06:00 +0000

    Remove unused constant

--- src/pvnet_app/consts.py
+++ src/pvnet_app/consts.py
@@ -1,9 +1,3 @@
 sat_path = "sat.zarr"
 nwp_ukv_path = "nwp_ukv.zarr"
 nwp_ecmwf_path = "nwp_ecmwf.zarr"
-
-
-uk_box = dict(
-    x_geostationary=[-996_133.85, -480_064.6],
-    y_geostationary=[4_512_606.3, 5_058_679.8],
-)
