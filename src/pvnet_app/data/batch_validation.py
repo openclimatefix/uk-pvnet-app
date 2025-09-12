beb0f676d19aa998ab2fc01b74396b29da1a3e52
James Fulton <djamesfulton@yahoo.co.uk>
2025-09-12 17:03:04 +0000

    bug fix

--- src/pvnet_app/data/batch_validation.py
+++ src/pvnet_app/data/batch_validation.py
@@ -10,13 +10,13 @@ def check_nwp_sources_not_all_zero(batch: NumpyBatch) -> None:
     Raises:
         ValueError: If the NWP data is all zeros
     """
-
-    for nwp_source in batch["nwp"].keys():
-        if (batch["nwp"][nwp_source]["nwp"] == 0).all():
-            raise ValueError(
-                f"NWP data for {nwp_source} is all zeros. This is probably an error. "
-                "To fix this check raw NWP data, and the nwp-consumer"
-            )
+    if "nwp" in batch:
+        for nwp_source in batch["nwp"].keys():
+            if (batch["nwp"][nwp_source]["nwp"] == 0).all():
+                raise ValueError(
+                    f"NWP data for {nwp_source} is all zeros. This is probably an error. "
+                    "To fix this check raw NWP data, and the nwp-consumer"
+                )
 
 
 def check_batch(batch: NumpyBatch) -> None:
