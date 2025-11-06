cec4c5d6d620eb609283bf0d82202542fd678030
Peter Dudfield <peter.dudfield@hotmail.com>
2025-11-06 18:12:40 +0000

    remove logging

--- src/pvnet_app/save.py
+++ src/pvnet_app/save.py
@@ -280,8 +280,6 @@ def get_forecast_values_from_dataarray(
 
     forecast_values = []
     for target_time in pd.to_datetime(da_gsp.target_datetime_utc.values):
-        logger.info(f"Target time: {target_time}")
-        logger.info(f"{da_gsp.target_datetime_utc.values}")
         da_gsp_time = da_gsp.sel(target_datetime_utc=target_time)
 
         target_time = target_time.replace(tzinfo=UTC)
