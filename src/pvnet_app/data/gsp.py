3c6cf72760872d21feb89224ed1bff427fd9c447
James Fulton <41546094+dfulu@users.noreply.github.com>
2026-02-04 17:50:50 +0000

    Hot-fix: Temporarily add backup capacities (#385)

--- src/pvnet_app/data/gsp.py
+++ src/pvnet_app/data/gsp.py
@@ -1,8 +1,19 @@
 """Functions to get GSP data from the database."""
+import logging
+from importlib.resources import files
+
 import numpy as np
 import pandas as pd
 from nowcasting_datamodel.connection import DatabaseConnection
 from nowcasting_datamodel.read.read_gsp import get_latest_gsp_capacities
+
+logger = logging.getLogger()
+
+
+BACKUP_CAPACITIES: pd.Series = pd.read_csv(
+    files("pvnet_app.data").joinpath("gsp_backup_capacities_2026_02_04.csv"),
+    index_col="gsp_id",
+)["capacity_mwp"]
 
 
 def get_gsp_and_national_capacities(
@@ -22,19 +33,32 @@ def get_gsp_and_national_capacities(
         - Pandas series of most recent GSP capacities
         - National capacity value
     """
-    with db_connection.get_session() as session:
-        # Get GSP capacities
-        all_capacities = get_latest_gsp_capacities(
-            session=session,
-            gsp_ids=[0, *gsp_ids],
-            datetime_utc=t0 - pd.Timedelta(days=2),
+    try:
+        with db_connection.get_session() as session:
+            # Get GSP capacities
+            all_capacities = get_latest_gsp_capacities(
+                session=session,
+                gsp_ids=[0, *gsp_ids],
+                datetime_utc=t0 - pd.Timedelta(days=2),
+            )
+
+        # Do basic sanity checking
+        if np.isnan(all_capacities).any():
+            raise ValueError("Capacities contain NaNs")
+
+        if len(all_capacities)!=len(gsp_ids):
+            raise ValueError("Not enough capacities returned")
+
+        national_capacity = all_capacities[0].item()
+        gsp_capacities = all_capacities[1:]
+
+    except Exception as e:
+        logger.error(f"Error in loading GSP capacities from database: {e}")
+        logger.warning(
+            "We couldnt load all the capacities from the database, "
+            "so we are using back up ones from 2026-02-04",
         )
-
-    # Do basic sanity checking
-    if np.isnan(all_capacities).any():
-        raise ValueError("Capacities contain NaNs")
-
-    national_capacity = all_capacities[0].item()
-    gsp_capacities = all_capacities[1:]
+        national_capacity = BACKUP_CAPACITIES.loc[0].item()
+        gsp_capacities = BACKUP_CAPACITIES.loc[gsp_ids]
 
     return gsp_capacities, national_capacity
